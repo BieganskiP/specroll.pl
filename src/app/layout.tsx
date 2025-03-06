f6a1b1a33ddc56089c6e7be61cb9384ab05fb387
bieganskip1996 <bieganski1996@gmail.com>
2025-03-06 07:41:37 +0100

    Add Vercel Analytics for website tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -73,6 +74,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
