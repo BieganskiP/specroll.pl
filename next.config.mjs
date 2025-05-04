99cfb21ac793eacdc1f3b30f589659e7769120ff
bieganskip1996 <bieganski1996@gmail.com>
2025-05-04 13:49:33 +0200

    Add redirects for www.specroll.pl to specroll.pl

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.specroll.pl",
+          },
+        ],
+        destination: "https://specroll.pl/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
